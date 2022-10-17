908d4dde0f76778cf284fa23fa958638917da175
S-Hanin <sign.rx@yandex.ru>
2022-10-17 16:46:36 +0300

    move driver operation to another class

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='SeleniumCrawler',
+    version='0.1',
+    packages=['spider', 'spider.tests'],
+    url='https://github.com/S-Hanin/SeleniumCrawler',
+    license='GPLv2',
+    author='S.Khanin',
+    author_email='',
+    description='Set of classes alowing to control browser and create chains of tasks (using url, xpath, cssquery, js)'
+)
--- spider/core.py
+++ spider/core.py
@@ -1,7 +1,10 @@
 # -*- coding: utf8 -*-
+"""
+Core logic
+"""
 import logging
 import types
-from typing import Union, Type
+from typing import Type, Optional
 
 from pyquery import PyQuery
 from selenium import webdriver
@@ -18,23 +21,38 @@ LOGGER = logging.getLogger(__name__)
 
 
 class SeleniumSpider:
+    """
+    Template class, supports task processing logic
+    """
 
     def __init__(self, driver_class: Type[WebDriver] = None, options=None):
         """
         :param driver_class: Driver to use (Chrome, Firefox, etc). Chrome is used by default
-        :param options: Specific options objects for the chosen driver (selenium.webdriver.chrome.options.Options)
+        :param options: Specific options objects for the chosen driver
+        (selenium.webdriver.chrome.options.Options)
         """
         self.__transport = BrowserTransport(driver_class or webdriver.Chrome,
                                             options or Options())
         self.__task_providers_chain: ProvidersChain = ProvidersChain(self.task_generator())
 
     def prepare(self, driver: WebDriver) -> None:
+        """
+        Allows to set up driver. Is executed before spider launches
+        :param driver: WebDriver
+        """
         pass
 
     def task_generator(self) -> types.GeneratorType:
+        """
+        Initial point to create Task(s)
+        :return: Generator
+        """
         yield
 
     def run(self) -> None:
+        """
+        Run crawling process
+        """
         with self.__transport as transport:
             self.prepare(transport.driver)
             self.__process_cycle()
@@ -45,21 +63,21 @@ class SeleniumSpider:
                 next_task_provider = self.__process_task(task)
                 self.__handle_next_task_provider(next_task_provider)
         except StopSpiderException as ex:
-            LOGGER.info("Spider stopped on StopSpiderException")
+            LOGGER.info("Spider stopped on StopSpiderException: %s", ex)
         except KeyboardInterrupt as ex:
-            LOGGER.info("Spider stopped on KeyboardInterrupt")
-
-    def __process_task(self, task: Task) -> Union[types.GeneratorType, None]:
-        if not task:
-            return
-        try:
-            task_result = self.__transport.request(task)
-            task_result_handler = getattr(self, f"task_{task.name}")
-            return task_result_handler(self.__transport.driver, PyQuery(task_result), task)
-        except NoSuchElementException as ex:
-            LOGGER.warning(ex)
+            LOGGER.info("Spider stopped on KeyboardInterrupt: %s", ex)
         except Exception as ex:
-            LOGGER.warning(ex)
+            LOGGER.critical(ex)
+
+    def __process_task(self, task: Task) -> Optional[types.GeneratorType]:
+        if task:
+            try:
+                task_result = self.__transport.request(task)
+                task_result_handler = getattr(self, f"task_{task.name}")
+                return task_result_handler(self.__transport.driver, PyQuery(task_result), task)
+            except NoSuchElementException as ex:
+                LOGGER.warning(ex)
+        return None
 
     def __handle_next_task_provider(self, task_provider: types.GeneratorType) -> None:
         # checking task_provider type because handler may return any type
